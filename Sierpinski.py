3e4202289aa260c63d5555aecc6b58d9be403899
Maze <invoker322sf@gmail.com>
2022-06-19 15:17:07 +1000

    created mvp

--- Sierpinski.py
+++ Sierpinski.py
@@ -5,9 +5,8 @@ import math
 class Sierpinski:
     def __init__(self, accuracy: int):
         self.accuracy = accuracy
-        self._x_array = [0.0]
-        self._y_array = [0.0]
-        self._create_arrays()
+        self._x_array = None
+        self._y_array = None
 
     def _add_next_point(self, x: float, y: float, number: int):
         match number:
@@ -21,8 +20,10 @@ class Sierpinski:
                 self._x_array.append((x + 0.5) / 2)
                 self._y_array.append((y + math.sqrt(3)) / 2.0)
 
-    def _create_arrays(self):
-        for i in range(self.accuracy):
+    def create_arrays(self):
+        self._x_array = [0.0]
+        self._y_array = [0.0]
+        for i in range(self.accuracy - 1):
             self._add_next_point(self._x_array[-1], self._y_array[-1], random.randint(0, 2))
 
     @property
@@ -32,6 +33,4 @@ class Sierpinski:
     @property
     def y_array(self):
         return self._y_array
-
-
 
--- main.py
+++ main.py
@@ -1,12 +1,106 @@
+import tkinter as tk
+from Sierpinski import Sierpinski
+import enum
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 
 
-from Sierpinski import Sierpinski
+class Color(enum.Enum):
+    blue = "b+"
+    red = "r+"
+    green = "g+"
+    cyan = "c+"
+    magenta = "m+"
+    yellow = "y+"
+    black = "k+"
 
-sierpinski = Sierpinski(accuracy=5000)
-x_array = sierpinski.x_array
-y_array = sierpinski.y_array
+
+class App:
+    def __init__(self):
+        self.sierpinski = None
+        self.root = tk.Tk()
+        self.root.title("Sierpinski Triangle")
+        self.root.geometry("500x500")
+        self.bg_color = "#FFF"
+        self.root['bg'] = self.bg_color
+
+        tk.Label(self.root, text="Accuracy", bg=self.bg_color).pack()
+
+        self.canvas = None
+        self.figure = None
+        self.axes = None
+
+        self.triangle_color = Color.black.value
+
+        self.accuracy = 60000
+        self.sierpinski = Sierpinski(self.accuracy)
+        self.sierpinski.create_arrays()
+
+        self.accuracy_var = tk.IntVar()
+
+        self.accuracy_scale = tk.Scale(self.root, from_=0, to=self.accuracy, resolution=1000, orient=tk.HORIZONTAL,
+                                       length=200, variable=self.accuracy_var, command=lambda _: self._draw_triangle(),
+                                       bg=self.bg_color, bd=0)
+        self.accuracy_scale.pack(pady=5)
+
+        self.settings = tk.Frame(self.root, borderwidth=3, bg=self.bg_color)
+        self.settings.pack()
+
+        self.triangle_color_name = tk.StringVar()
+        self._init_buttons()
+
+        self.x_array = self.sierpinski.x_array
+        self.y_array = self.sierpinski.y_array
+
+        self._draw_triangle()
+
+        self.root.mainloop()
+
+    def _init_buttons(self):
+        color_buttons = []
+        for color in Color:
+            radiobutton = tk.Radiobutton(self.settings,
+                                         text=color.name,
+                                         value=color.value,
+                                         bg=self.bg_color,
+                                         variable=self.triangle_color_name, command=lambda: self._change_color())
+            radiobutton.pack(side=tk.LEFT)
+            radiobutton.deselect()
+            color_buttons.append(radiobutton)
+        color_buttons[0].select()
+
+    def _change_color(self):
+        self.triangle_color = self.triangle_color_name.get()
+        self._draw_triangle()
+
+    def _init_triangle(self):
+        self.x_array = self.sierpinski.x_array
+        self.y_array = self.sierpinski.y_array
+        self._draw_triangle()
+
+    def _delete_triangle(self):
+        self.canvas.get_tk_widget().pack_forget()
+        plt.cla()
+        plt.close(self.figure)
+
+    def _configure_axes(self):
+        self.figure.set_figheight(20)
+        self.figure.set_figwidth(20)
+        plt.axis('off')
+
+    def _init_plt_items(self):
+        if self.canvas is not None:
+            self._delete_triangle()
+        self.figure, self.axes = plt.subplots()
+        self._configure_axes()
+
+    def _draw_triangle(self):
+        self._init_plt_items()
+        accuracy = self.accuracy_var.get()
+        self.axes.plot(self.x_array[0:accuracy], self.y_array[0:accuracy], self.triangle_color, markersize=0.1)
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self.root)
+        self.canvas.get_tk_widget().pack()
 
 
-plt.plot(x_array, y_array, "b+", markersize=0.1)
-plt.show()
+app = App()
